6551c98b512493cb5ef3bd5832e29a93c8c6f9d1
tottopyou <rgrabovsky2006@gmail.com>
2024-01-11 18:15:03 +0100

    the 2 part of 1 task

--- task_1/main.py
+++ task_1/main.py
@@ -1,6 +1,20 @@
 from data import *
+import re
+def word_to_number(word):
+    word_mapping = {
+        'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
+        'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
+    }
 
-splited_num = string.split()
+    pattern = '|'.join(re.escape(key) for key in word_mapping.keys())
+    regex = re.compile(pattern)
+
+    result = regex.sub(lambda match: str(word_mapping[match.group()]), word)
+    return result
+
+integer_number = word_to_number(string)
+
+splited_num = integer_number.split()
 
 array = []
 result_arr = []
