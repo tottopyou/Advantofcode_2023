7361d48e2e2b4774a9b18544aa7653a9bdbfe666
tottopyou <rgrabovsky2006@gmail.com>
2024-01-12 15:43:41 +0100

    the 2 part of 2 task

--- task_2/main.py
+++ task_2/main.py
@@ -5,27 +5,24 @@ game = games.split('\n')
 
 pattern = r'(\d+)\s*([a-zA-Z]+)'
 
-i = 0
-res = 0
-bool = True
+res = []
+power_set = 0
+red_ar,blue_ar,green_ar = [],[],[]
 
 for round in game:
-    i += 1
     matches = re.findall(pattern, round)
     numbers_and_colors = [(int(match[0]), match[1]) for match in matches]
     for data in numbers_and_colors:
-        if data[0] > 12 and data[1] == "red":
-            bool = False
-            break
-        elif data[0] > 13 and data[1] == "green":
-            bool = False
-            break
-        elif data[0] > 14 and data[1] == "blue":
-            bool = False
-            break
-    if bool == True:
-        res += i
-    bool = True
-print(res)
+        if data[1] == "red":
+            red_ar.append(data[0])
+        elif data[1] == "green":
+            green_ar.append(data[0])
+        elif data[1] == "blue":
+            blue_ar.append(data[0])
+    power_set = max(green_ar) * max(red_ar) * max(blue_ar)
+    red_ar, blue_ar, green_ar = [], [], []
+    res.append(power_set)
+
+print(sum(res))
 
 
