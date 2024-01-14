e29aaac2490a12526604436bf3d021507df20daf
tottopyou <rgrabovsky2006@gmail.com>
2024-01-14 18:44:54 +0100

    the 2 part of 4 task

--- task_4/main.py
+++ task_4/main.py
@@ -4,24 +4,35 @@ import re
 card_string = games.split('\n')
 
 win_card = []
-step = 0.5
 res = []
+game = {}
+copy_list = []
 
 for card in card_string:
     winning_numbers = re.findall(r'\b\d+\b', card.split('|')[0])
     your_numbers = re.findall(r'\b\d+\b', card.split('|')[1])
+    game[winning_numbers[0]] = 1
+
+
+for i, card in enumerate(card_string):
+    winning_numbers = re.findall(r'\b\d+\b', card.split('|')[0])
+    your_numbers = re.findall(r'\b\d+\b', card.split('|')[1])
     winning_numbers.pop(0)
-    print("Winning Numbers:", winning_numbers)
-    print("Your Numbers:", your_numbers)
+
     for num in your_numbers:
         if num in winning_numbers:
             win_card.append(num)
 
     if win_card:
         for win in win_card:
-            step *= 2
-        res.append(step)
-    win_card = []
-    step = 0.5
+            for _ in range(game[str(i+1)]):
+                copy_list.append(str(i+2))
+            i += 1
 
-print(sum(res))
+    for copy in copy_list:
+        game[copy] += 1
+
+    copy_list = []
+    win_card = []
+print(game.values())
+print(sum(game.values()))
