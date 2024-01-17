81325bc8c14dcc7989dd52649cb50c4d5b41b51a
tottopyou <rgrabovsky2006@gmail.com>
2024-01-17 17:42:34 +0100

    the 2 part of 8 task

--- task_8/main.py
+++ task_8/main.py
@@ -1,25 +1,44 @@
 from data import *
 import re
 
-direction = info.split('\n')[0]*100000
+direction = info.split('\n')[0]*1000000
 
 variants = info.split('\n')
 
 variants.pop(0)
 variants.pop(0)
 variant = {}
+answer = []
+j = 0
+bool = False
 
 for var in variants:
     variant[var[:3]] = var[7:10] + var[12:15]
 
-answer = "AAA"
+
+for item in variant.keys():
+    if item[2] == "A":
+        answer.append(item)
+        print(item)
 
 for i, direct in enumerate(direction):
-    if direct == 'L':
-        answer = variant[answer][:3]
-    elif direct == 'R':
-        answer = variant[answer][3:]
-    if answer == "ZZZ":
-        print(i+1)
+    for _ in range(len(answer)):
+        if direct == 'L':
+            answer.append(variant[answer[0]][:3])
+            answer.pop(0)
+        elif direct == 'R':
+            answer.append(variant[answer[0]][3:])
+            answer.pop(0)
+    for res in answer:
+        if res[2] == "Z":
+            j+= 1
+        if j == len(answer):
+            print(i+1)
+            bool = True
+            break
+    if bool == True:
+        print(answer)
         break
-
+    else:
+        print(j)
+        j = 0
