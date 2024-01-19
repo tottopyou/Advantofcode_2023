4c008fdcbdb698c79d5e56fcc4654cad026a1a82
tottopyou <rgrabovsky2006@gmail.com>
2024-01-19 22:42:06 +0100

    the 1 part of 21 task

--- task_21/main.py
+++ task_21/main.py
@@ -19,22 +19,23 @@ def solveMaze(maze):
         return None
 
     queue = deque()
-    steps = 64
     queue.appendleft((start[0], start[1], 0))
     directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
     visited = [[False] * C for _ in range(R)]
 
     while len(queue) != 0:
         coord = queue.pop()
-        steps -= 1
         visited[coord[0]][coord[1]] = True
+
+        if maze[coord[0]][coord[1]] == "E":
+            print(coord[2])
+            coord = queue.pop()
 
         for dir in directions:
             nr, nc = coord[0]+dir[0], coord[1]+dir[1]
             if (nr < 0 or nr >= R or nc < 0 or nc >= C or maze[nr][nc] == "#" or visited[nr][nc]): continue
             queue.appendleft((nr, nc, coord[2]+1))
 
-        if steps == 0:
-            print(len(queue)+1)
 
-solveMaze(maze)
+
+print(solveMaze(maze))
