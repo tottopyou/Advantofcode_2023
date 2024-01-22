29fffd755951d799070515d85d91cb5b4bf23e68
tottopyou <rgrabovsky2006@gmail.com>
2024-01-22 09:05:22 +0100

    Letter Combinations of a Phone Number

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 from typing import List
 
-
 class Solution:
-    def longestCommonPrefix(self, v: List[str]) -> str:
-        ans=""
-        v=sorted(v)
-        first=v[0]
-        last=v[-1]
-        for i in range(min(len(first),len(last))):
-            if(first[i]!=last[i]):
-                return ans
-            ans+=first[i]
-        return ans
+    def letterCombinations(self, digits: str) -> List[str]:
+        keys = {'1': '0', '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        res = []
+        for i, num in enumerate(digits):
+            if i < len(digits) - 1:  # Check if not the last digit
+                for val in keys[num]:
+                    for next_val in keys[digits[i+1]]:
+                        res.append(val + next_val)
+            elif len(digits) == 1:
+                for val in keys[num]:
+                    res.append(val)
+        return res
 
-
-print(Solution.longestCommonPrefix(0, ["flower", "flow", "flight"]))
+solution = Solution()
+result = solution.letterCombinations("23")
+print(result)
